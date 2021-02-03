0b0a2686c3b26d06ef457e06ce386728f278c88f
Muratcan Kara <muratfivem@gmail.com>
2021-02-03 23:47:08 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    //SOLID
+    //O : Open Closed Principle = Yaptıgın yazılıma yeni bir özellik ekliyorsan mevcuttaki hiçbir koduna dokunamazsın.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Entities.Abstract;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class : referans tip olabilir demek
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    //new() : newlenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null); // =null filtire vermeye de bilirsin demek
+        T Get(Expression<Func<T, bool>> filter); // filtre zorunlu
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
